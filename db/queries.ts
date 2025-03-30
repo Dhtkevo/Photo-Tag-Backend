936476557af24f4b1bfc97f4fd23538096fe4139
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2025-03-30 16:37:14 -0400

    Save coordinates of characters to be found in database

--- db/queries.ts
+++ db/queries.ts
@@ -1,1 +1,24 @@
 import { prisma } from "./prisma";
+
+async function populateDB() {
+  await prisma.targetCharacter.create({
+    data: {
+      name: "kyle",
+      x: 875,
+      y: 334,
+    },
+  });
+
+  await prisma.targetCharacter.create({
+    data: {
+      name: "john",
+      x: 776,
+      y: 551,
+    },
+  });
+
+  const users = await prisma.targetCharacter.findMany();
+  //console.log(users);
+}
+
+//populateDB();
